18177728f079d75f747dff720deb42542cd66ab6
Felipe Pereira Barros <64047005+Felipe-Pereira-Barros@users.noreply.github.com>
2020-05-11 14:10:26 -0300

    New code added (multiple variables & constant)

---
+++ Codes/multiple_variables_declarations_&_constant.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+#define pi 3.1415
+#define channel cout << "Felipe Pereira Barros\n\n";
+
+int main(){
+
+    //int LIFE, AMMO, HEALTH;
+    
+    /*int LIFE;
+    int AMMO;
+    int HEALTH;*/
+
+    int LIFE=3; int AMMO=500; int HEALTH=100;
+
+    cout << pi << "\n\n";
+    channel
+
+    system("pause"); 
+    return 0;
+}
