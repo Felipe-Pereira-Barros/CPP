cf8de7631182ab6569ba9d7a2611aeea7db3ba2c
Felipe Pereira Barros <64047005+Felipe-Pereira-Barros@users.noreply.github.com>
2020-05-13 13:03:19 -0300

    New code Added
    Increment and Decrement of variables

---
+++ Codes/Increment_&_Decrement_of_Variables.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    //n1=n1+1;
+    //n1=n1+10; n1+=10;
+    //n1=n1-10; n1-=10;
+    //n1=n1*10; n1*=10;
+    //n1=n1/10; n1/=10;
+    //n1++; POS
+    //++n1; PRE
+
+
+    int n1=10;
+
+    
+
+    cout << n1-- << "\n\n";
+    cout << n1 << "\n\n";
+
+
+
+    return 0;
+}
