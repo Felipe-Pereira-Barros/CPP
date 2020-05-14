f1854b52ee1f7d0f77ddb3ee731f1f2123ceb184
Felipe Pereira Barros <64047005+Felipe-Pereira-Barros@users.noreply.github.com>
2020-05-14 09:14:04 -0300

    New code added
    Inverting variable values

---
+++ Codes/Inverting_variable_values.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int num=10;
+
+    num=-num;
+
+    cout << num << "\n\n";
+
+    num = num * -1;
+
+    cout << -num << "\n\n";
+
+    num+=10;
+
+    cout << num << "\n\n";
+
+    return 0;
+}
