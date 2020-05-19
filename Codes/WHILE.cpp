7f1ae615d6d2c1a5b9200c6e17187e5ede682fad
Felipe Pereira Barros <64047005+Felipe-Pereira-Barros@users.noreply.github.com>
2020-05-19 08:48:31 -0300

    New code added
    WHILE function

---
+++ Codes/WHILE.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int count,count2;
+
+    count=0;
+    while (count<10)
+    {
+        cout << count << "\nFelipe Pereira Barros\n";
+        count++;
+        
+    }
+    
+    cout << "\nRoutine finished\n";
+
+    count2=20;
+    while (count2>0)
+    {
+        cout << count2 << "\nFelipe Pereira Barros\n";
+        count2--;
+        
+    }
+    
+    cout << "\nRoutine finished\n";
+
+    return 0;
+}
