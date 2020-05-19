59429997e9defd219c8d784b1cdd7745fe4fabec
Felipe Pereira Barros <64047005+Felipe-Pereira-Barros@users.noreply.github.com>
2020-05-19 09:03:56 -0300

    New code added

---
+++ Codes/WHILE_2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int count2;
+
+    count2=0;
+    while (count2++<20)
+    {
+        cout << count2 << "\nFelipe Pereira Barros\n";        
+    }
+    
+    cout << "\nRoutine finished\n";
+
+    return 0;
+}
