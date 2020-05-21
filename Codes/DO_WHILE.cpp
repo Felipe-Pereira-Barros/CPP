052ef6dcf8d9ef19d9ab39f07dc5f9e6a54f662f
Felipe Pereira Barros <64047005+Felipe-Pereira-Barros@users.noreply.github.com>
2020-05-21 12:29:22 -0300

    New code added
    DO WHILE

---
+++ Codes/DO_WHILE.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int count;
+
+    count=0;
+    do 
+    {
+        cout << "Felipe Pereira Barros" << count << "\n";
+        count++;
+    } 
+    while (count<20);
+    
+    cout << "\nRoutine Finished\n";
+
+    return 0;
+}
