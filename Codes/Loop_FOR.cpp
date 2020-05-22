d6cd437de8e22c00a49a6b5f6e56b4215c1a3b19
Felipe Pereira Barros <felipe.pereira.barros@hotmail.com>
2020-05-22 17:04:33 -0300

    code fixed

--- Codes/Loop_FOR.cpp
+++ Codes/Loop_FOR.cpp
@@ -4,9 +4,13 @@ using namespace std;
 
 int main()
 {
-
-
-
-
+    
+   int x,y,z;
+   
+   for (x=0,y=1,z=0;   x<=10 && z<=6  ;x++,y+=2,z+=2)
+   {
+       cout << x << " - " << y << " - " << z <<"\n";
+   }
+   
     return 0;
 }
