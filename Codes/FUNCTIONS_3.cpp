e506bb4c7d0219a5881526c9c33eac39d7a6dc3c
Felipe Pereira Barros <felipe.pereira.barros@hotmail.com>
2020-05-29 16:06:03 -0300

    New code added
    Argument omission

---
+++ Codes/FUNCTIONS_3.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+void print(string txt="Felipe Pereira Barros");
+
+int main()
+{
+
+    print();
+
+
+    return 0;
+}
+
+void print(string txt)
+{
+    cout << "\n" << txt << "\n";
+}
